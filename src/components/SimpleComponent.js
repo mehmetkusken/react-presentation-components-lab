88b468065a4dda4efa49166e1f704ba63e693848
Mehmet <mehmetkusken@gmail.com>
2021-10-20 12:18:01 +0200

    commit tree 035509565922d5add2bb287b8954a5eaf5f1fd6b

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
-// Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+	constructor(props) {
+		super(props = {mood: 'happy'});
+		this.state = {
+			mood: props.mood
+		}
+	}
+
+	handleClick = () => {
+		const mood = this.state.mood === 'happy' ? 'sad' : 'happy'
+		this.setState({
+			mood: mood
+		})
+	}
+
+	render() {
+		return (<div onClick={this.handleClick}>{this.state.mood}</div>)
+	}
+}
+
+export default SimpleComponent; 
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
-// Code SimplerComponent Here
+import React from 'react'
+
+
+
+
+const Button = ({handleClick}) => <div onClick={ handleClick }>"I am just happy"</div>;
+
+
+
+
+export default Button 
